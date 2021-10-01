d6085f9e1f28137705c78928057d94f3d7b5709e
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-10-01 11:40:27 +0200

    add backbone structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import ProjectListPage from "./pages/ProjectListPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import EditProjectPage from "./pages/EditProjectPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+function App(props) {
+  const [user, setUser] = useState(props.user);
+
+  const addUser = (user) => {
+    setUser(user);
+  };
+
+  console.log("App js: ", user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar user={user} setUser={addUser} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* <Route exact path="/projects" component={ProjectListPage} /> */}
+        {/* <Route
+          exact path="/projects"
+          render={props => {
+            if (user) {
+              return <ProjectListPage {...props} />
+            } else {
+              return <Redirect to="/" />
+            }
+          }}
+        /> */}
+        <ProtectedRoute
+          exact
+          path="/projects"
+          user={user}
+          component={ProjectListPage}
+        />
+        {/* <Route exact path="/projects/:id" component={ProjectDetailsPage} /> */}
+        <ProtectedRoute
+          exact
+          path="/projects/:id"
+          user={user}
+          component={ProjectDetailsPage}
+          redirect="/login"
+        />
+        <Route exact path="/projects/edit/:id" component={EditProjectPage} />
+        <Route
+          exact
+          path="/signup"
+          render={(props) => <Signup setUser={addUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login setUser={addUser} {...props} />}
+        />
+      </Switch>
     </div>
   );
 }
