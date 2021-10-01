0b0923a1da5b4a6f59352fd4aa597bec8d32a961
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-10-01 11:52:03 +0200

    add project

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { logout } from '../services/auth';
+import React from "react";
+import { Link } from "react-router-dom";
+import { logout } from "../services/auth";
 
 export default function Navbar(props) {
+  const handleLogout = () => {
+    logout().then(() => {
+      props.setUser(null);
+    });
+  };
 
-	const handleLogout = () => {
-		logout().then(() => {
-			props.setUser(null);
-		})
-	}
-
-	return (
-		<nav>
-			{props.user ? (
-				<>
-					<Link to="/">
-						<button>Home</button>
-					</Link>
-					<Link to="/projects">
-						<button>Projects</button>
-					</Link>
-					<Link to="/" onClick={() => handleLogout()}>
-						<button>Logout</button>
-					</Link>
-				</>
-			) : (
-				<>
-					<Link to="/signup">
-						<button>Signup</button>
-					</Link>
-					<Link to="/login">
-						<button>Login</button>
-					</Link>
-				</>
-			)}
-		</nav>
-	)
+  return (
+    <nav>
+      {props.user ? (
+        <>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>
+          <Link to="/" onClick={() => handleLogout()}>
+            <button>Logout</button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/signup">
+            <button>Signup</button>
+          </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </>
+      )}
+    </nav>
+  );
 }
